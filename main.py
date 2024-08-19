514cc56f38267b91a782ae153ab467b73aaa0d86
Raavi Kaur <raavi211299@gmail.com>
2024-08-19 18:26:30 -0500

    add the code of rock paper scissor game

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 paper = '''
     _______
 ---'   ____)____
@@ -22,6 +24,26 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
 game_images = [paper, rock, scissors]
-player_choice = int(input("Choose 0 for Paper, 1 for Rock, or 2 for Scissors:"))
-print("You chose:", game_images[player_choice])
+
+while True:
+    player_choice = int(input("Choose 0 for Paper, 1 for Rock, or 2 for Scissors:"))
+    if player_choice > 2 or player_choice < 0:
+        print("Invalid Option")
+    else:
+        print("You chose:", game_images[player_choice])
+        computer_choose = randint(0, 2)
+        print("Computer choose:", game_images[computer_choose])
+        if player_choice == computer_choose:
+            print("It's a Tie!")
+        elif player_choice == 0 and computer_choose == 1:
+            print("You won!")
+        elif player_choice == 1 and computer_choose == 2:
+            print("You won")
+        elif player_choice == 2 and computer_choose == 0:
+            print("You won")
+        else:
+            print("Computer win")
+    if input("Do you want to play again? (Y/N)").upper() != 'Y':
+        break
